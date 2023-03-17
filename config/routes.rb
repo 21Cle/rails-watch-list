759192b009df1778e5d4645955100078362b1fcb
clemence vert <clemence.legrand@yahoo.fr>
2023-03-17 13:28:29 +0100

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+    end
+    resources :bookmarks, only: :destroy
+  end
